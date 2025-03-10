470af6301353a38f84baf92ec7ec0348dd197588
Anurag <anuragom2002@gmail.com>
2025-03-10 16:01:52 +0530

    Added a new campground creation page and integrated navigation links in the campgrounds index page.

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ db.once("open", () => {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.render("home");
   //   res.send("Hello From Yelp Camp");
@@ -34,6 +37,16 @@ app.get("/", (req, res) => {
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
