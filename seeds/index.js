1b76aaea2117b29ec4ac3fecb4284827751f6af1
Anurag <anuragom2002@gmail.com>
2025-03-10 15:16:48 +0530

    Added seeds for database

--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
 const path = require("path");
-const Campground = require("./models/campground");
+const Campground = require("../models/campground");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 const db = mongoose.connection;
@@ -10,4 +12,22 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection erro:"));
 db.once("open", () => {
   console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  db.close();
 });
